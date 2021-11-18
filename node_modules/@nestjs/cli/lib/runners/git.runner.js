4383bce0a7e8b00de7a2db98dd9d6d8f1429efdd
Nick Mitchell <nickymitch@gmail.com>
2021-11-19 10:19:12 +1100

    fix: delete node_modules. Closes #2
    Node modules accidentally pushed before gitignore updated. Closes #2

--- node_modules/@nestjs/cli/lib/runners/git.runner.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const abstract_runner_1 = require("./abstract.runner");
-class GitRunner extends abstract_runner_1.AbstractRunner {
-    constructor() {
-        super('git');
-    }
-}
-exports.GitRunner = GitRunner;
